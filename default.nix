a11a72e9545288a7fe1a53ba662a044df7601e78
Lars Jellema <lars.jellema@gmail.com>
2018-10-19 16:46:41 +0200

    Use hex from nixpkgs

--- default.nix
+++ default.nix
@@ -3,20 +3,19 @@
 with pkgs;
 
 let
-  hex = fetchurl {
-    url = "https://repo.hex.pm/installs/1.6.0/hex-0.18.1.ez";
-    sha512 = "9c806664a3341930df4528be38b0da216a31994830c4437246555fe3027c047e073415bcb1b6557a28549e12b0c986142f5e3485825a66033f67302e87520119";
-  };
+  hex = pkgs.beam.packages.erlang.hex;
 in
 
 (callPackage (mixToNix ./.) {}).overrideAttrs (oldAttrs: rec {
   name = "educator_aaa";
   version = "0.0.1";
 
+  buildInputs = oldAttrs.buildInputs ++ [ hex ];
+
   buildPhase = ''
     runHook preBuild
 
-    mix archive.install --force ${hex}
+    addToSearchPath MIX_PATH "${hex}/lib/erlang/lib/"
     mix release --env=prod
 
     runHook postBuild
